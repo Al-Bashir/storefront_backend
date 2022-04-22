f57b6cb0ed271fc7163c003c8e2ca7af793d6ab4
Al-Bashir <Al-Bashir@users.noreply.github.com>
2022-04-22 03:41:42 +0200

    change routeshandeler to routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import routeController from '../routerHandlers/routeHandler';
+import routeController from '../routes/routeHandler';
 import errorHandler from '../middleware/error.handler';
 
 const app: express.Application = express();
@@ -7,7 +7,7 @@ const app: express.Application = express();
 app.use(express.json());
 
 app.use('/', routeController);
-// Handle unfound route
+// Handle false route
 
 // Error Handler middleware
 app.use(errorHandler);
